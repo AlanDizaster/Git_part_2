02c3f842e51ef78ab8b54193cf170f508f2c1af8
Anton Shatunov <shatunov00@gmail.com>
2020-12-20 19:31:07 +0300

    switch was add

--- Git_part_2/Git_part_2.cpp
+++ Git_part_2/Git_part_2.cpp
@@ -3,11 +3,59 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <clocale>
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	cout << ("hello world") << endl;
+	bool choice = true;
+	setlocale(LC_ALL, "rus");
+	while (choice == true)
+	{
+		char key;
+		cout << "+ - сложение" << endl << "- -вычетание " << endl << "* - умножения" << endl << "/ - деление" << endl << "Для выхода введиет quit" << endl;
+		cin >> key;
+		switch (key)
+		{
+		case '+':
+		{
+					system("cls");
+					cout << "Функция сложения в разработке" << endl;
+		}
+			break;
+
+		case '-':
+		{
+					system("cls");
+					cout << "Функция вычитания в разработке" << endl;
+		}
+			break;
+
+		case '*':
+		{
+					system("cls");
+					cout << "Функция умножения в разработке" << endl;
+		}
+			break;
+
+		case '/':
+		{
+					system("cls");
+					cout << "Функция деления в разработке" << endl;
+
+		}
+			break;
+
+		case 'q':
+			choice = false;
+			break;
+
+		default:
+			system("cls");
+			break;
+		}
+	}
+	
 	system("pause");
 	return 0;
 }
